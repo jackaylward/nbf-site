536f66a14109f10ca3dfc59ac2cf3cea9d1b1179
jackaylward <jack@aylwardsolutions.ie>
2026-09-17 13:38:11 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // Paste your Apps Script Web app URL (it ends in /exec) between the quotes:
-window.SITE_APP_API = "PASTE_YOUR_WEB_APP_LINK_HERE";
+window.SITE_APP_API = "https://script.google.com/macros/s/AKfycbyjZtfYdEpVttIrwIPOfdPaao0LR4MQT8Bu1gFi93XtCusF0RyPyZyelnfk3JfFqCtAlg/exec";
